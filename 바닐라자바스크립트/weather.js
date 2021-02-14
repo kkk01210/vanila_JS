797c659252dddf394957d0f6e339858ec53bb36c
kkk01210 <77380084+kkk01210@users.noreply.github.com>
2021-02-14 13:34:59 +0900

    바닐라아이스크림

---
+++ 바닐라자바스크립트/weather.js
@@ -0,0 +1,64 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "a7ed9a63ec4f0bd5c170854a3143f2d7";
+const COORDS = 'coords';
+
+
+function getWeather(lat, lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+    .then(function(response){
+       return response.json();
+    })
+    .then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj))
+}
+
+
+function handleGeoSucces(position){
+    const latitude = position. coords. latitude;
+    const longitude = position. coords. longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude)
+}
+
+function hadnleGeoError(){
+    console.log('Cant access geo location')
+}
+
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, hadnleGeoError)
+}
+
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    }
+    else {
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords. latitude, parseCoords. longitude);
+    }
+}
+
+
+
+function init(){
+    loadCoords();
+}
+
+
+
+init();
